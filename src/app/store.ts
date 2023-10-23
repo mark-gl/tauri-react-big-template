d6b15aa8dbbeddf5f5f73fb497c0caa0c60b9c32
mark-gl <26033115+mark-gl@users.noreply.github.com>
2023-10-23 22:10:39 +0100

    Set up redux-first-history for router history

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,23 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "redux";
+import { createReduxHistoryContext } from "redux-first-history";
+import { createBrowserHistory } from "history";
+
+const { createReduxHistory, routerMiddleware, routerReducer } =
+  createReduxHistoryContext({
+    history: createBrowserHistory(),
+    reduxTravelling: true
+  });
 
 export const store = configureStore({
-  reducer: {
+  reducer: combineReducers({
+    router: routerReducer,
     counter: persistReducer({ key: "counter", storage }, counterReducer)
-  }
+  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(routerMiddleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
@@ -19,3 +31,5 @@ export type AppThunk<ReturnType = void> = ThunkAction<
 >;
 
 export const persistor = persistStore(store);
+
+export const history = createReduxHistory(store);
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { persistor, store } from "./app/store";
+import { persistor, history, store } from "./app/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
+import { HistoryRouter } from "redux-first-history/rr6";
 import App from "./App";
 import "./styles.css";
 
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <HistoryRouter history={history}>
           <App />
-        </BrowserRouter>
+        </HistoryRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
