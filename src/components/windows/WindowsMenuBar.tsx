067403884f2d6818c699526df9eb4b7784897336
mark-gl <26033115+mark-gl@users.noreply.github.com>
2023-10-23 23:29:37 +0100

    Add Vite SVG plugin and forward/back buttons

--- src/components/windows/WindowsMenuBar.tsx
+++ src/components/windows/WindowsMenuBar.tsx
@@ -1,9 +1,47 @@
 import { WindowsControls } from "./WindowsControls";
 import styles from "./WindowsMenuBar.module.css";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { goBack, goForward } from "redux-first-history";
+import LeftArrow from "../../assets/arrow-left-solid.svg?react";
+import RightArrow from "../../assets/arrow-right-solid.svg?react";
+import { useEffect, useState } from "react";
 
 export function WindowsMenuBar() {
+  const dispatch = useAppDispatch();
+  const history = useAppSelector((state) => state.router);
+  const [backEnabled, setBackEnabled] = useState(false);
+  const [forwardEnabled, setForwardEnabled] = useState(false);
+
+  useEffect(() => {
+    setBackEnabled(window.history.length > 1 && window.history.state.idx > 0);
+    setForwardEnabled(
+      window.history.length > 1 &&
+        window.history.length - 1 != window.history.state.idx
+    );
+  }, [history]);
+
   return (
     <div className={styles.menuBar}>
+      <div className={styles.navigation}>
+        <LeftArrow
+          title="Back"
+          className={`${styles.arrow} ${
+            backEnabled ? "" : `${styles.arrowDisabled}`
+          }`}
+          onClick={() => {
+            if (backEnabled) dispatch(goBack());
+          }}
+        />
+        <RightArrow
+          title="Forward"
+          className={`${styles.arrow} ${
+            forwardEnabled ? "" : `${styles.arrowDisabled}`
+          }`}
+          onClick={() => {
+            if (forwardEnabled) dispatch(goForward());
+          }}
+        />
+      </div>
       <div className={styles.dragRegion} data-tauri-drag-region></div>
       <WindowsControls />
     </div>
