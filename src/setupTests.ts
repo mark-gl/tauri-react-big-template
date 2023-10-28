cd190713c5deb2748338253f88d4b09e76059cb3
mark-gl <26033115+mark-gl@users.noreply.github.com>
2023-10-28 21:15:37 +0100

    Mock 'window.matchMedia' usage during tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,11 @@
 /// <reference types="vitest/globals" />
 import "@testing-library/jest-dom";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation(() => ({
+    matches: false,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn()
+  }))
+});
