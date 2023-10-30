60cb3252c4f25244382a5727dd1f149d006be77d
mark-gl <26033115+mark-gl@users.noreply.github.com>
2023-10-30 23:34:42 +0000

    Remove option for system window controls on Mac
    Removing window decorations hides the controls completely on Mac. I don't know of a way to change the titleBarStyle/hiddenTitle settings without restarting the app, so I've removed the option.

--- src/routes/settings/AppearancePage.tsx
+++ src/routes/settings/AppearancePage.tsx
@@ -9,11 +9,24 @@ import {
   setWindowDecorations
 } from "../../features/config/configSlice";
 import styles from "./AppearancePage.module.css";
+import { useEffect, useState } from "react";
+import { type } from "@tauri-apps/api/os";
 
 export function AppearancePage() {
   const dispatch = useAppDispatch();
   const currentTheme = useAppSelector(selectTheme);
   const windowDecorations = useAppSelector(selectWindowDecorations);
+  const [isMac, setIsMac] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    async function initialise() {
+      if (isMac === null) {
+        const osType = await type();
+        setIsMac(osType == "Darwin");
+      }
+    }
+    if (isTauri()) initialise();
+  }, [isMac]);
 
   const handleThemeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     dispatch(setTheme(event.target.value));
@@ -38,7 +51,7 @@ export function AppearancePage() {
           </option>
         ))}
       </select>
-      {isTauri() && (
+      {isTauri() && isMac === false && (
         <div>
           <input
             className={styles.checkbox}
